e3406418c8e46b8b471eda1c8c4437ea418776b2
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 01:22:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function addingEventListener() {
-  alert('I was clicked!');
+  const input = document.getElementById('input');
+  input.addEventListener('click', addingEventListener() {
+    alert('I was clicked!');
+  });
 }
